72f32b4924d5027483f4bf6bde6b3c70a300ee6a
Andrey O <ozzy.ext@gmail.com>
2021-08-05 19:42:49 +0300

    Change docker reestr to github

--- ci/publish.bat
+++ ci/publish.bat
@@ -6,13 +6,13 @@ echo "Build project ..."
 dotnet publish ..\src\MyLab.AsyncProcessor.Api\MyLab.AsyncProcessor.Api.csproj -c Release -o .\out\app
 
 echo "Build image '%1' and 'latest'..."
-docker build -t ozzyext/mylab-async-proc-api:%1 -t ozzyext/mylab-async-proc-api:latest .
+docker build -t ghcr.io/mylab-search-fx/indexer:%1 -t ghcr.io/mylab-search-fx/indexer:latest .
 
 echo "Publish image '%1' ..."
-docker push ozzyext/mylab-async-proc-api:%1
+docker push ghcr.io/mylab-search-fx/indexer:%1
 
 echo "Publish image 'latest' ..."
-docker push ozzyext/mylab-async-proc-api:latest
+docker push ghcr.io/mylab-search-fx/indexer:latest
 
 goto done
 
