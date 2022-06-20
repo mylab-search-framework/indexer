b887965ed808862c20ae84d5ac7d5e2b13e55637
Andrey O <ozzy.ext@gmail.com>
2022-06-20 19:55:34 +0300

    Add tests for IndexerOptions

---
+++ src/UnitTests/IndexerOptionsBehavior.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using MyLab.Search.Indexer.Options;
+using Xunit;
+
+namespace UnitTests
+{
+    public class IndexerOptionsBehavior
+    {
+        [Fact]
+        public void ShouldSearchIndexOptions()
+        {
+            //Arrange
+            var indexOpt = new IndexOptions
+            {
+                Id = "foo"
+            };
+            var opts = new IndexerOptions
+            {
+                Indexes = new[]
+                {
+                    indexOpt
+                }
+            };
+
+            //Act
+            var foundOpts = opts.GetIndexOptions("foo");
+
+            //Assert
+            Assert.Equal(indexOpt, foundOpts);
+        }
+
+        [Fact]
+        public void ShouldFailIfIndexOptsNotFound()
+        {
+            //Arrange
+            var opts = new IndexerOptions();
+
+            //Act & Assert
+            Assert.Throws<IndexOptionsNotFoundException>(() => opts.GetIndexOptions("foo"));
+        }
+
+        [Theory]
+        [InlineData("pre-", "-post", "pre-bar-post")]
+        [InlineData(null, "-post", "bar-post")]
+        [InlineData("pre-", null, "pre-bar")]
+        [InlineData(null, null, "bar")]
+        public void ShouldProvideEsIndexName(string prefix, string postfix, string expected)
+        {
+            //Arrange
+            var indexOpt = new IndexOptions
+            {
+                Id = "foo",
+                EsIndex = "bar"
+            };
+            var opts = new IndexerOptions
+            {
+                EsIndexNamePrefix = prefix,
+                EsIndexNamePostfix = postfix,
+                Indexes = new[]
+                {
+                    indexOpt
+                }
+            };
+
+            //Act
+            var foundName = opts.GetEsIndexName("foo");
+
+            //Assert
+            Assert.Equal(expected, foundName);
+        }
+    }
+}
