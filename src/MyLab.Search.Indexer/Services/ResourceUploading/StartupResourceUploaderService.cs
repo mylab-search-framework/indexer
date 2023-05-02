89e9108764bb3920f3d02d6cb5086890cdc96480
Andrey O <ozzy.ext@gmail.com>
2023-05-02 17:55:08 +0300

    Add resource uploaders in startup service

--- src/MyLab.Search.Indexer/Services/ResourceUploading/StartupResourceUploaderService.cs
+++ src/MyLab.Search.Indexer/Services/ResourceUploading/StartupResourceUploaderService.cs
@@ -3,26 +3,45 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using MyLab.Log.Dsl;
 
 namespace MyLab.Search.Indexer.Services.ResourceUploading
 {
     class StartupResourceUploaderService : BackgroundService
     {
         private readonly IResourceUploader[] _uploaders;
+        private readonly IDslLogger _log;
 
         public StartupResourceUploaderService(
-            IServiceProvider serviceProvider)
+            IServiceProvider serviceProvider,
+            ILogger<StartupResourceUploaderService> logger = null)
         {
-            _uploaders = new[]
+            _uploaders = new IResourceUploader[]
             {
-                ActivatorUtilities.CreateInstance<LifecyclePolicyUploader>(serviceProvider)
+                ActivatorUtilities.CreateInstance<LifecyclePolicyUploader>(serviceProvider),
+                ActivatorUtilities.CreateInstance<IndexTemplateUploader>(serviceProvider),
+                ActivatorUtilities.CreateInstance<ComponentTemplateUploader>(serviceProvider)
             };
+
+            _log = logger.Dsl();
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             foreach (var uploader in _uploaders)
-                await uploader.UploadAsync(stoppingToken);
+            {
+                try
+                {
+                    await uploader.UploadAsync(stoppingToken);
+                }
+                catch (Exception e)
+                {
+                    _log?.Error("Resources uploading error", e)
+                        .AndFactIs("uploader", uploader.GetType().Name)
+                        .Write();
+                }
+            }
         }
     }
 }
