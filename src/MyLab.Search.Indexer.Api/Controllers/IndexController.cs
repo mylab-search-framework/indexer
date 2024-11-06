ed1560949f5cac46676a48682e5c8db2da637671
Andrey O. <ozzy.ext@gmail.com>
2024-11-06 23:30:03 +0300

    Add api project

---
+++ src/MyLab.Search.Indexer.Api/Controllers/IndexController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyLab.Search.Indexer.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IndexController : ControllerBase
+    {
+        private readonly ILogger<IndexController> _logger;
+
+        public IndexController(ILogger<IndexController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
