5d2f68539139e0956a1ec78b02a66564b0ce0ab0
Andrey O. <ozzy.ext@gmail.com>
2024-11-15 12:13:23 +0300

    Add empty controller + application project

--- src/MyLab.Search.Indexer.Api/Controllers/IndexController.cs
+++ src/MyLab.Search.Indexer.Api/Controllers/IndexController.cs
@@ -1,9 +1,10 @@
+using System.Text.Json.Nodes;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MyLab.Search.Indexer.Api.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("indexes")]
     public class IndexController : ControllerBase
     {
         private readonly ILogger<IndexController> _logger;
@@ -11,6 +12,38 @@ namespace MyLab.Search.Indexer.Api.Controllers
         public IndexController(ILogger<IndexController> logger)
         {
             _logger = logger;
+        }
+
+        [HttpPut("{idx_id}/{doc_id}")]
+        public async Task<IActionResult> Put
+        (
+            [FromRoute(Name="idx_id")] string idxId, 
+            [FromRoute(Name = "doc_id")] string docId, 
+            [FromBody]JsonObject document
+        )
+        {
+
+        }
+
+        [HttpPatch("{idx_id}/{doc_id}")]
+        public async Task<IActionResult> Patch
+        (
+            [FromRoute(Name = "idx_id")] string idxId,
+            [FromRoute(Name = "doc_id")] string docId,
+            [FromBody] JsonObject document
+        )
+        {
+
+        }
+
+        [HttpPatch("{idx_id}/{doc_id}")]
+        public async Task<IActionResult> Delete
+        (
+            [FromRoute(Name = "idx_id")] string idxId,
+            [FromRoute(Name = "doc_id")] string docId
+        )
+        {
+
         }
     }
 }
