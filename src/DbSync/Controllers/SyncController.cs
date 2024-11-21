ddf810ba883b64355d1d5b967965343317a915b4
Andrey O. <ozzy.ext@gmail.com>
2024-11-21 05:28:39 +0300

    Add DbSync project

---
+++ src/DbSync/Controllers/SyncController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DbSync.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SyncController : ControllerBase
+    {
+        
+    }
+}
